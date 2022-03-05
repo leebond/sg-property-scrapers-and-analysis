6ed1570ad3074ce95eef39bd02417f5de979c5b3
Guanhua Lee <leebond_guanhua@hotmail.com>
2022-03-06 00:01:02 +0800

    added throttle, verbose edit, closed logic loop

--- scrape-resale-prices/CondoLanded/scrape-private-property-resale-historical-prices.py
+++ scrape-resale-prices/CondoLanded/scrape-private-property-resale-historical-prices.py
@@ -6,8 +6,7 @@ Created on Sat Oct 17 17:22:13 2020
 """
 
 
-import re, os, sys
-import itertools
+import re, os, sys, time, random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -77,8 +76,9 @@ if __name__=='__main__':
     if len(sys.argv) < 2:
         print('please provide run type >python this_script.py "new" or "continue"')
         sys.exit(0)
+    
     mode = sys.argv[1]
-    print(mode)
+    print('Mode selected: %s' %mode)
     driver = webdriver.Chrome("C:/Users/LEEbo/Documents/chromedriver/chromedriver.exe")
     http = 'https://www.ura.gov.sg/realEstateIIWeb/transaction/search.action'   
     driver.get(http)
@@ -97,15 +97,20 @@ if __name__=='__main__':
     if not os.path.exists('output'):
         os.makedirs('output')
 
-
+    ### two runtime modes: new or continue
+    ### continue will carry on the loop from the project in the latest saved file
     if mode == 'new':
         start_count = 0
         saved_projects = ['new']
+        print("Warning: Runtime will take about 2-3 hours.")
     elif mode == 'continue':
         # print(projects_names)
-        output_filepath = './output/Private_*.xlsx'
-        files = glob(output_filepath)
-        latest_file = files[-1]
+        # output_filepath = './output/Private_*.xlsx'
+        output_filepath = os.path.join('output','Private_*.xlsx')
+        # files = glob(output_filepath)
+        list_of_files = glob(output_filepath)
+        # latest_file = files[-1]
+        latest_file = max(list_of_files, key=os.path.getctime)
         print(latest_file)
         saved_df = pd.read_excel(latest_file)
         saved_projects = saved_df['Project Name'].unique().tolist()
@@ -113,7 +118,11 @@ if __name__=='__main__':
         print(last_saved_project)
         print(len(saved_projects))
         start_count = projects_names.index(last_saved_project)
+    else:
+        sys.exit(0)
+        driver.close()
     
+    ### loop through all the project names
     for i in tqdm(range(start_count, len(projects))):
         # first_project = projects[0]
         # first_project.click()
@@ -123,17 +132,9 @@ if __name__=='__main__':
         
         
         print(i, projects[i].text)
-        # WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, projects[i]))).click()
-        # WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, projects[i+1]))).click()
-        # WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, projects[i+2]))).click()
-        # WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, projects[i+3]))).click()
-        # WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, projects[i+4]))).click()
+
         projects[i].click()
-        # projects[i+1].click()
-        # projects[i+2].click()
-        # projects[i+3].click()
-        # projects[i+4].click()
-        
+       
         submit_btn_xpath = '/html/body/div/div[4]/div[4]/form/div/div[1]/p[1]/input'
         submit_btn = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, submit_btn_xpath)))
         submit_btn.click()
@@ -171,7 +172,8 @@ if __name__=='__main__':
             print('%s records found' %project_tbl_df.shape[0])
             df = df.append(project_tbl_df, ignore_index=True)
         
-        if i % 20 == 0:
+        ### save file at every n records
+        if i % 100 == 0:
             if not os.path.exists('output'):
                 os.makedirs('output')
             df.to_excel(f"./output/Private_resale_prices_{datetime.now().date()}{saved_projects[-1]}.xlsx", index=False)
@@ -182,6 +184,11 @@ if __name__=='__main__':
         modify_search_btn = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, modify_search_xpath)))
         modify_search_btn.click()
         
-        
+        ran_val = random.randint(1,3)
+        print('Sleep for %s secs...' %ran_val)
+        time.sleep(ran_val)
     
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    df.to_excel(f"./output/Private_resale_prices_{datetime.now().date()}{saved_projects[-1]}.xlsx", index=False)
     # driver.close()
